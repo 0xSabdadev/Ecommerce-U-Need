b717e82c0c0d00cbfc591f4707f73858738b1d06
sabdadev <jasonalhilal@gmail.com>
2021-01-16 11:55:33 +0700

    Ecommerce App v1.0.1

---
+++ lib/screens/splash/components/splash_content.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+import '../../../constants.dart';
+import '../../../size_config.dart';
+
+class SplashContent extends StatelessWidget {
+  const SplashContent({
+    Key key,
+    this.text,
+    this.image,
+  }) : super(key: key);
+  final String text, image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Spacer(
+          flex: 2,
+        ),
+        Text(
+          "U-Need",
+          style: TextStyle(
+            fontSize: getProportionateScreenWidth(39),
+            color: kPrimaryColor,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(
+          text,
+          textAlign: TextAlign.center,
+        ),
+        Spacer(flex: 2),
+        Image.asset(
+          image,
+        ),
+      ],
+    );
+  }
+}
